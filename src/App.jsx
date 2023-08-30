d4d27c55ee7788d5dd2cb968c05cd95aa7086925
Danish <dan71ish@gmail.com>
2023-08-31 00:53:59 +0530

    Footer done

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@ const DiscordAd = lazy(() => import("./components/DiscordAd"));
 const Navbar = lazy(() => import("./components/Navbar"));
 const Hero = lazy(() => import("./components/Hero"));
 import "./index.css";
-import './style.css'
+import "./style.css";
 import Stats from "./components/Stats";
-const Features = lazy(() => import('./components/Features'))
-const Flexiblity = lazy(() => import('./components/Flexiblity'))
+import Footer from "./components/Footer";
+const Features = lazy(() => import("./components/Features"));
+const Flexiblity = lazy(() => import("./components/Flexiblity"));
 function App() {
   return (
     <>
       <Suspense fallback={<div className="text-center">...</div>}>
         <DiscordAd />
         <Navbar />
+        <Hero />
       </Suspense>
-      <Hero />
       <Features />
       <Flexiblity />
       <Stats />
+      <Footer />
     </>
   );
 }
